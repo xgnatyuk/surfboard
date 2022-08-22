9cfcc92e33ff50fee80253e5c22ab3fe33380b64
Ksenia Gnatyuk <xgnatyuk@gmail.com>
2022-08-22 16:33:09 +0300

    script

---
+++ script.js
@@ -0,0 +1,31 @@
+class FullMenu {
+    constructor(selector) {
+        this.menu = document.querySelector(selector)
+        document.addEventListener('click', (e) => {
+            const target = e.target.closest('[data-menu]')
+            if (target) {
+                const event = target.dataset.menu
+                this[event]()
+            }
+        })
+    }
+    open() {
+        this.menu.classList.add('open')
+    }
+    close() {
+        this.menu.classList.remove('open')
+    }
+}
+var menu = new FullMenu('#full-menu')
+
+
+
+const targetDiv = document.getElementById("element");
+const parametres = document.getElementById("parametres-btn");
+parametres.onclick = function() {
+    if (targetDiv.style.display == "none") {
+        targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
+    }
+};
